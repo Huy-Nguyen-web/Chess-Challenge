dded451598881ed1bd9dd57d55f1702e998a0589
Huy Nguyen <77337321+Huy-Nguyen-web@users.noreply.github.com>
2023-07-24 13:38:48 +0700

    (~) Add move ordering MVVLVA Value

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,9 +4,9 @@ using System;
 public class MyBot : IChessBot {
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
     public Move Think(Board board, Timer t) {
-        Move[] moves = board.GetLegalMoves();
+        //Move[] moves = board.GetLegalMoves();
 
-        Move moveToPlay = Search(4, int.MinValue, int.MaxValue, board).Item2;
+        Move moveToPlay = Search(5, int.MinValue, int.MaxValue, board).Item2;
         return moveToPlay;
     }
 
@@ -31,48 +31,48 @@ public class MyBot : IChessBot {
         int pieceEval = whiteEval - blackEval;
         //// if black is to move, then it should be black advantage, then it's should be positive if the player is black
         int perspective = (board.IsWhiteToMove) ? 1 : -1;
-        int checkValue = board.IsInCheck()? 1000 : 0;
+        //int checkValue = board.IsInCheck()? 1000 : 0;
 
 
         int finalEval = pieceEval;
 
-        return finalEval * perspective;
+        return finalEval;
     }
 
-    //TODO: Finish odering move algorithm and add it
-    int SearchAllCapture(int alpha, int beta, Board board) {
-        int eval = Evaluate(board);
-        if(eval >= beta) {
-            return beta;
+    int GetMVVLVAValue(Move move) {
+        if (move.IsCapture) {
+            return (pieceValues[(int)move.CapturePieceType] - pieceValues[(int)move.MovePieceType]);
         }
-        alpha = Math.Max(alpha, eval);
-
-        Move[] moves = board.GetLegalMoves(true);
-        
-        foreach(Move move in moves) {
-            board.MakeMove(move);
-        }
-        
-        return alpha;
+        return -pieceValues[(int) move.MovePieceType];
     }
 
+    Move[] OrderMoveByMVVLVA(Move[] moves) {
+        Array.Sort(moves, (move1, move2) => {
+            int mvvLvaValue1 = GetMVVLVAValue(move1);
+            int mvvLvaValue2 = GetMVVLVAValue(move2);
+            return mvvLvaValue2.CompareTo(mvvLvaValue1);
+        });
+        return moves;
+    }
 
     Tuple<int, Move> Search(int depth, int alpha, int beta, Board board) {
         if (depth == 0) {
-            return Tuple.Create(Evaluate(board), Move.NullMove);
+            return Tuple.Create(Evaluate(board), Move.NullMove); 
         }
 
         Move[] moves = board.GetLegalMoves();
+        Move[] orderMoves = OrderMoveByMVVLVA(moves);
+
         Move bestMove = Move.NullMove;
         if (moves.Length == 0) {
             if (board.IsInCheck()) {
-                return Tuple.Create(int.MinValue, bestMove);
+                return Tuple.Create(int.MinValue, Move.NullMove);
             }
-            return Tuple.Create(0, bestMove);
+            return Tuple.Create(0, Move.NullMove);
         }
         if (board.IsWhiteToMove) {
             int maxEval = int.MinValue;
-            foreach (Move move in moves) {
+            foreach (Move move in orderMoves) {
                 board.MakeMove(move);
                 int currentEval = Search(depth - 1, alpha, beta, board).Item1;
                 board.UndoMove(move);
@@ -88,7 +88,7 @@ public class MyBot : IChessBot {
             return Tuple.Create(maxEval, bestMove);
         } else {
             int minEval = int.MaxValue;
-            foreach (Move move in moves) {
+            foreach (Move move in orderMoves) {
                 board.MakeMove(move);
                 int currentEval = Search(depth - 1, alpha, beta, board).Item1;
                 board.UndoMove(move);
@@ -102,7 +102,5 @@ public class MyBot : IChessBot {
             }
             return Tuple.Create(minEval, bestMove);
         }
-
-
     }
 }
